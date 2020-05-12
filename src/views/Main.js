8203f7274cc0cc277e7e734de4ab5422f047bef6
Almeeida <rodrigofigueire19@gmail.com>
2020-05-12 01:04:59 +0100

    refactor: removed gradient bg and updated links

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from '../../assets/logo.gif';
-import './style.css';
 
-const [discord, github, steam, youtube] = [
+import logo from '../assets/logo.png';
+import './Main.css';
+
+const [discord, github, steam, youtube, twitch] = [
   'https://discordapp.com/users/385132696135008259',
   'https://github.com/Almeeida',
   'https://steamcommunity.com/id/almeidaa01',
   'https://www.youtube.com/channel/UC9hiS7zuW1DdvyEmY7wsJKg',
+  'https://www.twitch.tv/almeida0001',
 ];
 
 /**
@@ -16,11 +18,12 @@ const [discord, github, steam, youtube] = [
 function Main() {
   return (
     <div className="main">
-      <img src={logo} className="hvr-bounce-in" alt="Almeida" /><br /><br />
-      <h1 className="hvr-buzz-out">Almeida</h1><br /><br />
-      <a href={discord} className="ico hvr-grow"><i className="fab fa-discord"></i></a>
-      <a href={github} className="ico hvr-grow"><i className="fab fa-github"></i></a>
+      <img src={logo} className="hvr-bounce-in" alt="Almeida" /><br/><br/>
+      <h1 className="hvr-buzz-out">Almeida</h1><br/><br/>
       <a href={youtube} className="ico hvr-grow"><i className="fab fa-youtube"></i></a>
+      <a href={twitch} className="ico hvr-grow"><i className="fab fa-twitch"></i></a>
+      <a href={github} className="ico hvr-grow"><i className="fab fa-github"></i></a>
+      <a href={discord} className="ico hvr-grow"><i className="fab fa-discord"></i></a>
       <a href={steam} className="ico hvr-grow"><i className="fab fa-steam"></i></a>
     </div>
   );
